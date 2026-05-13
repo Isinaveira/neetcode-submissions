9519362485567009366e53d7c15c65e0be14a2cb
Isi Naveira <isinaveira@gmail.com>
2026-05-14 00:08:29 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while(left < right) {
+
+            let mid = Math.floor((left + right) / 2);
+            //comprueba la derecha si es decreciente
+            if(nums[mid] > nums [right]){
+                left = mid + 1;
+            } else {
+                //comprueba si hay un cambio de tendencia
+                if(nums[left-1] > nums[left]){
+                    return nums[left];
+                }else{
+                //si no lo hay, es que está a la izquierda el menor    
+                    right = mid;
+            }
+        }
+    }
+    return nums[left];
+    }
+}
